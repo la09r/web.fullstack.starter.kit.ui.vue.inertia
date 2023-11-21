46cadbe216c169b7c149bb4841b5c772d37ee2c1
Alexander Gorchakov <alexander.gorchakov.leto@gmail.com>
2023-11-21 13:30:26 +0500

    fix Welcome page

---
+++ src/resources/js/components/Dashboard/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm mb-4">
+
+    </nav>
+    <Toast />
+    <ConfirmDialog />
+</template>
+
+<script setup>
+
+</script>
+<script>
+    export default {
+        data() {
+
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
